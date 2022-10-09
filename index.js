c05fbe107e19f1ed0d587d83a36737d1d75be6bd
Dipokal HHJ <hhj@devent.kr>
2022-10-09 14:00:37 +0900

    feat: add interpoint straight line
    + 점간 직선을 추가

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let start = { x: 50,    y: 200  };
-let end =   { x: 250,   y: 200 };
+let end =   { x: 450,   y: 200 };
 let cp = {
     "cp1": { x: 230,   y: 30  },
     "cp2": { x: 150,   y: 80  }
@@ -15,8 +15,8 @@ let move = {
 
 document.addEventListener('mousemove', (event) => {
     if (move.target !== 'none') {
-        document.querySelector(`#${move.target}`).style.top = `${event.clientY}px`
-        document.querySelector(`#${move.target}`).style.left = `${event.clientX}px`
+        document.querySelector(`#${move.target}`).style.top = `${event.clientY-5}px`
+        document.querySelector(`#${move.target}`).style.left = `${event.clientX-5}px`
 
         cp[move.target].x = event.clientX
         cp[move.target].y = event.clientY
@@ -54,6 +54,14 @@ function update() {
     ctx.arc(start.x, start.y, 5, 0, 2 * Math.PI);
     ctx.arc(end.x, end.y, 5, 0, 2 * Math.PI);
     ctx.fill();
+
+    ctx.beginPath();
+    ctx.strokeStyle = 'black';
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(cp1.x-5, cp1.y-5);
+    ctx.moveTo(end.x, end.y);
+    ctx.lineTo(cp2.x-5, cp2.y-5);
+    ctx.stroke();
 
     ctx.beginPath();
     ctx.fill();
